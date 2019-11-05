c2e7fa413793f5a600f8c87baa290acfbdde72e7
Gary <liujialing95@163.com>
2019-11-05 17:36:55 +0800

    add countup.js

--- src/views/CaseStudy/Vizient/index.js
+++ src/views/CaseStudy/Vizient/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import TheStory from 'components/TheStory'
 
 export default () => {
+  const theStoryData = {
+    story: [
+      'Vizient is the largest member-driven, health care performance improvement company in the country.',
+      'With dozens of products offered, Vizient tasked us with designing and building a new UI Toolkit to homogenize their user interface components across all services.'
+    ],
+    dataList: [
+      {
+        number: 5,
+        name: 'Products'
+      },
+      {
+        number: 10,
+        name: 'Team members'
+      },
+      {
+        number: 250,
+        suffix: '+',
+        name: 'App Screens'
+      }
+    ]
+  }
+  
   return (
     <div>
       vizient
+      <TheStory {...theStoryData}></TheStory>
     </div>
   )
 }
