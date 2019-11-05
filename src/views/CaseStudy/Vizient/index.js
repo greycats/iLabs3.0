13e30fa5ade85194fa37462c6b5188da17265c27
biao <iambillzhao@gmail.com>
2019-11-05 17:38:06 +0800

    update vizient page

--- src/views/CaseStudy/Vizient/index.js
+++ src/views/CaseStudy/Vizient/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Controller, Scene } from 'react-scrollmagic'
 
 export default () => {
   return (
     <div>
       vizient
+      <div>
+        <Controller>
+          <Scene duration={600} pin>
+            <div>Sticky Example</div>
+          </Scene>
+        </Controller>
+      </div>
     </div>
   )
 }
