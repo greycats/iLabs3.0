22402b7da6267fcbbcf3e02d9797d628e5013f4c
biao <iambillzhao@gmail.com>
2019-11-08 12:01:51 +0800

    fix animate issue

--- src/components/LeftTextRightAnimation/index.js
+++ src/components/LeftTextRightAnimation/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import CommonTitle from 'components/CommonTitle'
 import { SectionTitleWrap } from 'components/Styled'
-import { Controller, Scene } from 'react-scrollmagic'
 import { Tween, Timeline } from 'react-gsap'
 import { getTriggerId } from 'utils'
 
