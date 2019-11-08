184082374461011f70b6f22327c60d6ecf6b4114
biao <iambillzhao@gmail.com>
2019-11-08 12:01:16 +0800

    fix animate issue

--- src/components/LeftTextRightAnimation/index.js
+++ src/components/LeftTextRightAnimation/index.js
@@ -1,81 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CommonTitle from 'components/CommonTitle'
 import { SectionTitleWrap } from 'components/Styled'
 import { Controller, Scene } from 'react-scrollmagic'
-import { Tween } from 'react-gsap'
+import { Tween, Timeline } from 'react-gsap'
 import { getTriggerId } from 'utils'
 
+import ScrollMagic from 'scrollmagic'
+
+const controller = new ScrollMagic.Controller()
+
+const AnimateTitle = () => (
+  <Timeline
+    target={
+      <SectionTitleWrap>
+        <div className="title-wrap">
+          <CommonTitle title='Vizient Components for the Web' titleMargin='50px' />
+        </div>
+      </SectionTitleWrap>
+    }
+  >
+    <Tween
+      playState='play'
+      from={{
+        css: {
+          height: 0
+        }
+      }}
+      to={{
+        css: {
+          height: '94px'
+        }
+      }}
+    >
+    </Tween>
+  </Timeline>
+)
+
+const AnimateText = () => (
+  <Tween
+    from={{
+      opacity: 0,
+      top: '15px'
+    }}
+    to={{
+      opacity: 1,
+      top: '20px'
+    }}
+    delay={0.5}
+  >
+    <div style={{
+      width: '500px',
+      position: 'absolute',
+      left: '0'
+    }}>
+      <p>
+        The Toolkit system includes the resources needed to create user interfaces consistent with the Vizient’s product principles, design language, and best practices.
+      </p>
+      <br />
+      <p>
+        Rather than focusing on pixels, developers can focus on the application logic, while designers can focus on user experiences, interactions, and flows.
+      </p>
+    </div>
+  </Tween>
+)
+
 export default ({ id = '__trigger_scene' }) => {
+  const [playAnimation, setPlayAnimation] = useState(false)
+
+  const setAnimation = (elementSelector) => {
+    new ScrollMagic.Scene()
+      .triggerElement(elementSelector)
+      .addTo(controller)
+      .on('start', e => {
+        setPlayAnimation(true)
+      })
+  }
+
+  useEffect(() => {
+    setAnimation(getTriggerId(id))
+  }, [])
+
   return (
     <div className="layout-1240 panel challenge-wrap" style={{ minHeight: '600px' }}>
       <div id={id}></div>
       <div className="title-part">
-        <Controller>
-          <Scene
-            triggerElement={getTriggerId(id)}
-            duration={500}
-            reverse={false}
-          >
-            {(progress) => (
-              <Tween
-                from={{
-                  height: 0
-                }}
-                to={{
-                  height: '94px'
-                }}
-                totalProgress={progress}
-                paused
-              >
-                <SectionTitleWrap>
-                  <div className="title-wrap">
-                    <CommonTitle title='Vizient Components for the Web' titleMargin='50px' />
-                  </div>
-                </SectionTitleWrap>
-              </Tween>
-            )}
-          </Scene>
-        </Controller>
+        {
+          playAnimation ? <AnimateTitle /> : null
+        }
         <div style={{
           position: 'relative'
         }}>
-          <Controller>
-            <Scene
-              triggerElement={getTriggerId(id)}
-              duration={500}
-              reverse={false}
-            >
-              {(progress) => (
-                <Tween
-                  from={{
-                    opacity: 0,
-                    top: '15px'
-                  }}
-                  to={{
-                    opacity: 1,
-                    top: '20px'
-                  }}
-                  paused
-                  totalProgress={progress}
-                  delay={1500}
-                >
-                  <div style={{
-                    width: '500px',
-                    position: 'absolute',
-                    left: '0'
-                  }}>
-                    <p>
-                      The Toolkit system includes the resources needed to create user interfaces consistent with the Vizient’s product principles, design language, and best practices.
-                 </p>
-                    <br />
-                    <p>
-                      Rather than focusing on pixels, developers can focus on the application logic, while designers can focus on user experiences, interactions, and flows.
-                </p>
-                  </div>
-                </Tween>
-              )}
-            </Scene>
-          </Controller>
+         {
+           playAnimation ? <AnimateText /> : null
+         }
         </div>
       </div>
     </div>
