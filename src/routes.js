dbc5f491ed0de3602bb0d18d48a2dff3e4b141be
biao <iambillzhao@gmail.com>
2019-11-26 14:43:19 +0800

    add ourwork

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,14 @@
 import React from 'react'
-import { Router, Route, Switch } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
 import { AnimatedSwitch } from 'react-router-transition'
-// import { renderRoutes } from 'react-router-config'
 import history from './history'
 import HomeView from './views/Home'
 import CaseStudy from './views/CaseStudy'
-// import SignIn from './views/SignIn'
-// import SignUpRoutes from './views/SignUp/routes'
-// import ResetPwdRoutes from './views/ResetPwd/routes'
-// import OnboardingRoutes from './views/OnBoarding/routes'
-// import DashboardRoutes from './views/Dashboard/routes'
-// import DelegateRoutes from './views/Delegate/routes'
-// import StaticPagesRoutes from './views/Static/routes'
-// import Board from './views/Board/routes'
-// import StaticRouter from './staticViews/routes'
-
-// const routerMap = [
-// ...SignUpRoutes,
-// ...ResetPwdRoutes,
-// ...OnboardingRoutes,
-// ...DashboardRoutes,
-// ...DelegateRoutes,
-// ...StaticPagesRoutes,
-// ...StaticRouter,
-// ...Board
-// ]
+import OurWork from './views/OurWork'
 
 
 const routes = (
   <Router history={history}>
-    {/* <Switch> */}
     <AnimatedSwitch
       atEnter={{ opacity: 0 }}
       atLeave={{}}
@@ -50,8 +29,14 @@ const routes = (
           />
         )
       }} />
+      <Route path="/ourwork" render={(props) => {
+        return (
+          <OurWork
+            {...props}
+          />
+        )
+      }} />
     </AnimatedSwitch>
-    {/* </Switch> */}
   </Router>
 )
 
