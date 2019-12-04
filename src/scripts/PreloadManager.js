946541ed22c93f664a66e8a320a70509babc39c0
biao <iambillzhao@gmail.com>
2019-12-04 19:28:59 +0800

    update max of preload

--- src/scripts/PreloadManager.js
+++ src/scripts/PreloadManager.js
@@ -1,7 +1,7 @@
 import 'preload-js'
 import _ from 'lodash'
 const queue = new window.createjs.LoadQueue(true)
-queue.setMaxConnections(60)
+queue.setMaxConnections(1000)
 queue.maintainScriptOrder = false
 
 export const getImage = (id, use64 = false) => use64 ? queue.getResult(id, true) : _.get(queue.getResult(id), 'src', '')
