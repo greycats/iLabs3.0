a0e62bd9453987c7ab5c0ed7e7bc2e3f74542580
biao <iambillzhao@gmail.com>
2019-12-05 11:11:04 +0800

    add work section

--- src/views/Home/Mobile/index.js
+++ src/views/Home/Mobile/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './Banner'
+import Work from './Work'
 
 export default () => {
   return (
     <>
     <Banner></Banner>
+    <Work></Work>
     </>
   )
 }
