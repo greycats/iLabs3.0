60afec85dc97c9f3bfa5fcfaf483d663d3afa3e3
biao <iambillzhao@gmail.com>
2019-12-06 19:56:01 +0800

    add a temp loading animation..

---
+++ src/components/TempLoading/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import './style.css'
+
+export default () => <div class="lds-dual-ring"></div>
--- src/routes.js
+++ src/routes.js
@@ -8,12 +8,19 @@ const HomeView = lazyImport(import('./views/Home'))
 const CaseStudy = lazyImport(import('./views/CaseStudy'))
 const OurWork = lazyImport(import('./views/OurWork'))
 const Contact = lazyImport(import('./views/Contact'))
+import Loading from 'components/TempLoading'
 
 const routes = (
   <Suspense fallback={(() => {
     return (
-      <div>
-        ...
+      <div style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Loading/>
       </div>
     )
   })()}>
