d2bb28fd7d8c4792f85dae1f19e40a1838dfe89c
biao <iambillzhao@gmail.com>
2019-12-06 20:23:27 +0800

    optimize loading speed

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Banner from './Banner'
 import OurClients from './OurClients'
 import HowWeWork from './HowWeWork'
@@ -7,8 +7,31 @@ import Footer from 'components/Footer'
 import Work from './Work'
 
 import MobileHome from './Mobile'
+import greatSchool from 'data/great.js'
 
 export default () => {
+  const getData = async () => {
+    await greatSchool()
+  }
+  useEffect(() => {
+    getData()
+    Promise.all([
+      import('views/CaseStudy/GS'),
+      import('views/CaseStudy/GS/Questions'),
+      import('views/CaseStudy/GS/TheChallenge'),
+      import('views/CaseStudy/GS/Presentation'),
+      import('views/CaseStudy/GS/FirstLook'),
+      import('views/CaseStudy/GS/Progressive'),
+      import('views/CaseStudy/GS/Others')
+    ]).then(() => [
+      import('views/CaseStudy'),
+      import('views/OurWork'),
+      import('views/Contact'),
+      import('views/CaseStudy/Vizient'),
+      import('views/CaseStudy/Dcom'),
+      import('views/CaseStudy/Crew'),
+    ])
+  }, [])
   return (
     <>
       {
