64702c0d2d4db84c12cfd20f91b76bb19065e465
biao <iambillzhao@gmail.com>
2020-01-08 11:46:22 +0800

    fix menu issue

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -67,12 +67,12 @@ const MenuList = ({ data, onItemClick }) => {
     data.map((item, index) => {
       return (
         <div
-        key={index}
-        style={{
-          fontSize: window.isPC ? '64px' : '32px',
-          height: window.isPC ? '80px' : '20px',
-          marginBottom: '50px'
-        }}>
+          key={index}
+          style={{
+            fontSize: window.isPC ? '64px' : '32px',
+            height: window.isPC ? '80px' : '20px',
+            marginBottom: '50px'
+          }}>
           <span style={{
             cursor: 'pointer'
           }} onClick={() => {
@@ -91,7 +91,7 @@ const MenuList = ({ data, onItemClick }) => {
   )
 }
 
-const Contact = ({isPC}) => {
+const Contact = ({ isPC }) => {
   return (
     <div className="footer-wrap">
       <div className="footer">
@@ -138,15 +138,68 @@ const Contact = ({isPC}) => {
 const MobileContact = () => {
   return (
     <div className="footer-wrap">
-    <div className="footer">
-      <div className="footer-list">
-            <div className="icon-list">
-              <a href="https://www.linkedin.com/company/interactive-labs-inc/about" target="_blank"><img src={IconIn} /></a>
-              <a href="https://dribbble.com/InteractiveLabs" target="_blank"><img src={IconDribbble} /></a>
-            </div>
-        <div className="list-title">Copyright © 2019 interactivelabs.co</div>
+      <div className="footer">
+        <div className="footer-list">
+          <div className="icon-list">
+            <a href="https://www.linkedin.com/company/interactive-labs-inc/about" target="_blank"><img src={IconIn} /></a>
+            <a href="https://dribbble.com/InteractiveLabs" target="_blank"><img src={IconDribbble} /></a>
+          </div>
+          <div className="list-title">Copyright © 2019 interactivelabs.co</div>
+        </div>
       </div>
     </div>
+  )
+}
+
+const Content = ({ showMenu, dispatch }) => {
+  return (
+    <div
+      className="content"
+      style={{
+        display: window.isPC ? 'flex' : 'block',
+        height: '100vh',
+        width: '80%',
+        margin: '0 auto',
+        paddingTop: '20vh'
+      }}>
+      {
+        showMenu ?
+          <img src={logo} className="logo-img" alt="" onClick={() => {
+            showMenuAction(dispatch, false)
+            isMenuOpenAction(dispatch, false)
+            menuDirectionAction(dispatch, -1)
+            setTimeout(() => {
+              if (window.location.pathname !== '/') {
+                history.push('/')
+              }
+            })
+          }} /> : null
+      }
+      <div
+        style={{
+          width: window.isPC ? '40%' : '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          justifyContent: 'flex-start'
+        }}>
+        <MenuList data={menuData} onItemClick={() => {
+          showMenuAction(dispatch, false)
+          isMenuOpenAction(dispatch, false)
+          menuDirectionAction(dispatch, -1)
+        }} />
+      </div>
+      <div
+        style={{
+          width: window.isPC ? '60%' : '100%',
+          position: window.isPC ? 'unset' : 'absolute',
+          bottom: window.isPC ? 'unset' : '60px',
+        }}>
+        {
+          window.isPC ?
+            <Contact isPC={window.isPC} /> : <MobileContact />
+        }
+      </div>
     </div>
   )
 }
@@ -157,56 +210,15 @@ export default () => {
 
   return (
     <MenuContainer className={showMenu ? 'show' : ''}>
-      <AnimateBanner height={window.innerHeight} backgroundColor={0x323232} >
-        <div
-          className="content"
-          style={{
-            display: window.isPC ? 'flex' : 'block',
-            height: '100vh',
-            width: '80%',
-            margin: '0 auto',
-            paddingTop: '20vh'
-          }}>
-          {
-            showMenu ?
-              <img src={logo} className="logo-img" alt="" onClick={() => {
-                showMenuAction(dispatch, false)
-                isMenuOpenAction(dispatch, false)
-                menuDirectionAction(dispatch, -1)
-                setTimeout(()=> {
-                  if (window.location.pathname !== '/') {
-                    history.push('/')
-                  }
-                })
-              }} /> : null
-          }
-          <div
-            style={{
-              width: window.isPC ? '40%' : '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'flex-start',
-              justifyContent: 'flex-start'
-            }}>
-            <MenuList data={menuData} onItemClick={() => {
-              showMenuAction(dispatch, false)
-              isMenuOpenAction(dispatch, false)
-              menuDirectionAction(dispatch, -1)
-            }} />
+      {
+        window.isPC
+          ?
+          <AnimateBanner height={window.innerHeight} backgroundColor={0x323232} >
+            <Content showMenu={showMenu} dispatch={dispatch} />
+          </AnimateBanner>
+          : <div style={{ height: '100vh', background: '#323232' }}>
+            <Content showMenu={showMenu} dispatch={dispatch} />
           </div>
-          <div
-            style={{
-              width: window.isPC ? '60%' : '100%',
-              position: window.isPC ? 'unset' : 'absolute',
-              bottom: window.isPC ? 'unset' : '60px',
-            }}>
-              {
-                window.isPC ?
-                <Contact isPC={window.isPC}/> : <MobileContact/>
-              }
-          </div>
-        </div>
-      </AnimateBanner>
       }
     </MenuContainer>
   )
